db49a8ce9bc9a408276618e87100f8c3a6b3cd56
Evan Haut <evanwhaut@gmail.com>
2021-05-29 14:58:12 -0700

    no debig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,4 @@
-// As is with no custom configuration
-module.exports = require('@studiometa/tailwind-config');
-
-// By merging the package's configuration with you own custom one
-const config = require('@studiometa/tailwind-config');
-const merge = require('lodash.merge');
-
-module.exports = merge(config, {
+module.exports ={
   theme: {
-    debugOutline: true,
-    filter: { // defaults to {}
-      'none': 'none',
-      'grayscale': 'grayscale(1)',
-      'invert': 'invert(1)',
-      'sepia': 'sepia(1)',
-    },
-    backdropFilter: { // defaults to {}
-      'none': 'none',
-      'blur': 'blur(20px)',
-    },
   },
-  variants: {
-    filter: ['responsive'], // defaults to ['responsive']
-    backdropFilter: ['responsive'], // defaults to ['responsive']
-  },
-  plugins: [
-    require('tailwind-filter-utilities'),
-  ],
-});
+};
